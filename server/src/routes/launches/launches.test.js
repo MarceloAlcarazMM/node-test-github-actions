82d2d2f2d4314c5e413741c1d7d618a5460ef35b
Marcelo Alcaraz <marcelo.alcaraz@mentormate.com>
2024-03-22 14:24:19 -0300

    fix failing test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,11 +2,13 @@ const request = require('supertest');
 const app = require('../../app');
 
 const { mongoConnect, mongoDisconnect } = require('../../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API test', () =>{
 
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async ()=> {
